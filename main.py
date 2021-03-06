9f5aa2037f7dd73682fc1fc77180131f733d3b8a
MusadhiQ <63378124+musadhiq@users.noreply.github.com>
2021-03-06 03:54:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -35,19 +35,26 @@ elif option==4:
     sha384=hash_object.hexdigest()
     print("sha384 hash : "+sha384)
     print("  ")
-else:
+elif option==5:
     text = input("Enter Your Text :")
     hash_object = hashlib.sha256(text.encode())
     sha256 = hash_object.hexdigest()
     print("sha256 hash : "+ sha256)
+    print("   ")
     hash_object = hashlib.md5(text.encode())
     md5_hash = hash_object.hexdigest()
-    print("md5 hash  :" + md5_hash)
+    print("md5 hash    : " + md5_hash)
+    print("   )
     hash_object = hashlib.sha1(text.encode())
     sha1_hash = hash_object.hexdigest()
-    print("sha1 hash : " + sha1_hash)
+    print("sha1 hash   : " + sha1_hash)
+    print("   ")
     hash_object = hashlib.sha384(text.encode())
     sha384 = hash_object.hexdigest()
-    print("sha384 :"+sha384)
+    print("sha384 hash : "+sha384)
+    print("  ")
+          
+else:
+          print("Invalid input")
 
 print("Thank You!!..")
